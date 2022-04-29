852689ece876bb08723f6e67ab4fcd28f6a70d90
Melian <79233450803@yandex.ru>
2022-04-29 18:14:16 +0700

    Создала необходимые поля для заметки

--- app/src/main/java/Models/Notes.java
+++ app/src/main/java/Models/Notes.java
@@ -1,11 +1,26 @@
 package Models;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
 
 @Entity (tableName = "notes")
 public class Notes implements Serializable {
 
+    @PrimaryKey (autoGenerate = true)
+    int ID = 0;
 
+    @ColumnInfo (name = "title")
+    String title = "";
+
+    @ColumnInfo (name = "notes")
+    String notes = "";
+
+    @ColumnInfo (name = "data")
+    String data = "";
+
+    @ColumnInfo (name = "pinned")
+    boolean pinned = false;
 }
